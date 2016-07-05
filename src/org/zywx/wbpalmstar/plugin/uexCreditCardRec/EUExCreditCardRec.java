0bece78a2385ec192cfeec1ebf07ddc3657144ea
fred <fei.ye@3g2win.com>
2016-07-05 11:39:54 +0800

    uexCreditCardRec 4.0 插件出新

--- src/org/zywx/wbpalmstar/plugin/uexCreditCardRec/EUExCreditCardRec.java
+++ src/org/zywx/wbpalmstar/plugin/uexCreditCardRec/EUExCreditCardRec.java
@@ -1,15 +1,16 @@
 package org.zywx.wbpalmstar.plugin.uexCreditCardRec;
 
-import io.card.payment.CardIOActivity;
-import io.card.payment.CreditCard;
-
-import org.zywx.wbpalmstar.engine.EBrowserView;
-import org.zywx.wbpalmstar.engine.universalex.EUExBase;
-
 import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
-import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.zywx.wbpalmstar.engine.EBrowserView;
+import org.zywx.wbpalmstar.engine.universalex.EUExBase;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 public class EUExCreditCardRec extends EUExBase {
 
@@ -20,6 +21,7 @@ public class EUExCreditCardRec extends EUExBase {
 		super(context, inparent);
 	}
 
+    private String callbackFuncId = null;
 	@Override
 	protected boolean clean() {
 		return false;
@@ -32,6 +34,9 @@ public class EUExCreditCardRec extends EUExBase {
 		if (!TextUtils.isEmpty(params[0])) {
 			onScanPress(params[0]);
 		}
+        if(params.length == 2) {
+            callbackFuncId = params[1];
+        }
 	}
 
 	private void onScanPress(String token) {
@@ -41,8 +46,8 @@ public class EUExCreditCardRec extends EUExBase {
 		// required for authentication with card.io
 		scanIntent.putExtra(CardIOActivity.EXTRA_APP_TOKEN, token);
 		// customize these values to suit your needs.
-		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default:
-		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true); // default:
+		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, false); // default:
+		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, false); // default:
 		scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_ZIP, false); // default:
 		scanIntent.putExtra(CardIOActivity.EXTRA_USE_CARDIO_LOGO, true); // default:
 		// false
@@ -64,7 +69,7 @@ public class EUExCreditCardRec extends EUExBase {
 					.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
 			// Never log a raw card number. Avoid displaying it, but if
 			// necessary use getFormattedCardNumber()
-			resultStr = "Card Number: " + scanResult.getFormattedCardNumber()
+            resultStr = "Card Number: " + scanResult.getFormattedCardNumber()
 					+ "\n";
 			// Do something with the raw number, e.g.:
 			// myService.setCardNumber( scanResult.cardNumber );
@@ -82,22 +87,33 @@ public class EUExCreditCardRec extends EUExBase {
 			if (scanResult.zip != null) {
 				resultStr += "Zip: " + scanResult.zip + "\n";
 			}
-			String jsCallBack = func_on_cbLockAppState + "('"
-					+ scanResult.getFormattedCardNumber() + "','"
-					+ scanResult.expiryMonth + "/" + scanResult.expiryYear
-					+ "','" + scanResult.cvv + "')";
-			onCallback(jsCallBack);
+            if (null != callbackFuncId) {
+                try {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("cardNumber", scanResult.getFormattedCardNumber());
+                    callbackToJs(Integer.parseInt(callbackFuncId), false, jsonObject);
+                }catch (JSONException e) {
 
-			jsCallBack = cbCreditCardFunName + "('" + scanResult.getRedactedCardNumber() + "','" + scanResult.expiryMonth + "/" + scanResult.expiryYear + "','" + scanResult.cvv + "')";
-            		onCallback(jsCallBack);
+                }
+            } else {
+                String jsCallBack = cbCreditCardFunName + "('" + scanResult.getFormattedCardNumber() + "','" + scanResult.expiryMonth + "/" + scanResult.expiryYear + "','" + scanResult.cvv + "')";
+                onCallback(jsCallBack);
+            }
 		} else {
-			resultStr = "Scan was canceled.";
-			String jsCallBack = func_on_cbLockAppState + "('0','0','0')";
-			onCallback(jsCallBack);
+            if (null != callbackFuncId) {
+                try {
+                    JSONObject jsonObject = new JSONObject();
+                    jsonObject.put("cardNumber", 0);
+                    callbackToJs(Integer.parseInt(callbackFuncId), false, jsonObject);
+                }catch (JSONException e) {
 
-			jsCallBack = cbCreditCardFunName + "('0','0','0')";
-            		onCallback(jsCallBack);
+                }
+            } else {
+                resultStr = "Scan was canceled.";
+                String jsCallBack = cbCreditCardFunName + "('0','0','0')";
+                onCallback(jsCallBack);
+            }
+
 		}
-		Log.i("uexCreditCardRec", resultStr);
 	}
 }
